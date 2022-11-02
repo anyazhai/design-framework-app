718c24a833c75068fc8b1e30acf61270ef4c2038
anyazhai <chaithanyachaithanya2708@gmail.com>
2022-11-02 18:29:19 +0530

    navbar created

---
+++ src/components/navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../assets/base.css';
+import '../assets/index.css';
+
+export default function Navabar(){
+    return(
+        <nav className='nav-bar'>
+            <div className="container-nav">
+            <div className="brand">
+                <h2>Design System</h2>
+            </div>
+            <div>
+                <ul>
+                    <li><a href="#" className="nav-item">Setup</a></li>
+                    <li><a href="#" className="nav-item">Components</a></li>
+                </ul>
+            </div>
+        </div>
+        </nav>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from './components/navbar'
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <div>
+        <Navbar/>
+    </div>
+)
